9ebae0f0e16aba662f2918c7ffae2e00ec86a4d9
paaatcha <pacheco.comp@gmail.com>
2023-11-20 21:07:09 +0000

    a

--- webhooker.py
+++ webhooker.py
@@ -3,13 +3,13 @@ import os
 
 app = FastAPI()
 
-@app.post("/update")
+@app.post("/webhook/update")
 def update_site():
     print("Atualizando o site. Executando o comando git pull")
     os.system("git pull")    
 
 
-@app.get("/it-works")
+@app.get("/webhook/it-works")
 def it_works():
     print("It works!")
     return {"message": "It works!"}
